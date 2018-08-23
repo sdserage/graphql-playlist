e540ef8e0078a7aaf0201731e663d2185cdf7d9b
Scott Serage <xttocsurge51@gmail.com>
2018-08-23 17:01:15 -0600

    Updated the app to include the graphql schema and updated the schema to include a root query

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const PORT = 3210;
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+  schema,
 }));
 
 app.listen(PORT, () => {
