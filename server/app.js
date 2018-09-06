26d8f9028af4ada69e591692369c63b19e179237
Scott Serage <xttocsurge51@gmail.com>
2018-09-06 16:29:17 -0600

    Successfully connected to mLab
    Created and mLab account and a MongoDB instance on it to use for the tutorial. Successfully connected to that MongoDB instance.

--- server/app.js
+++ server/app.js
@@ -2,9 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const PORT = 3210;
 const app = express();
+
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
