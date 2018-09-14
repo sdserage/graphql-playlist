63a5f5d3b786f33b46cd93663bf4857d4353ce6e
sdserage <xttocsurge51@gmail.com>
2018-09-14 13:43:12 -0600

    Added cors and implemented it in the server

--- server/app.js
+++ server/app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const PORT = 3210;
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
 
 mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
