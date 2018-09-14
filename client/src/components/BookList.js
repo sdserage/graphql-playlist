e56252714caf527357cc852532ab02261e0eba03
sdserage <xttocsurge51@gmail.com>
2018-09-14 13:42:45 -0600

    Added GraphQL query and graphql from react-apollo

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 class BookList extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div id="main">
+      <div>
         <ul id="book-list">
           <li>Book name</li>
         </ul>
@@ -12,4 +24,4 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
