2947ba4c1933114efabe0230b4f891e520912e20
sdserage <xttocsurge51@gmail.com>
2018-09-15 22:44:42 -0600

    Created a queries file to store our queries separately from the components, moved the queries out of the components and instead imported them

---
+++ client/src/queries/queries.js
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-boost';
+
+export const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+export const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+export default {getAuthorsQuery, getBooksQuery};
